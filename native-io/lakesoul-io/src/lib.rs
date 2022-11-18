d93e53c4dce26306da30f7e84b2d68e844653fec
dmetasoul01 <opensource@dmetasoul.com>
2022-11-18 20:19:28 +0800

    refactor: separate fetcher, combiner, merger

--- native-io/lakesoul-io/src/lib.rs
+++ native-io/lakesoul-io/src/lib.rs
@@ -1,6 +1,10 @@
 #![feature(new_uninit)]
 #![feature(is_some_with)]
+#![feature(async_closure)]
+#![feature(let_chains)]
 
 pub mod lakesoul_reader;
 pub mod sorted_stream_merger;
-pub mod merge_handler;
+pub mod merge_traits;
+pub mod non_unique_fetcher;
+pub mod min_heap_combiner;
